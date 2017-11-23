4869599a295ea4624208b20d49321579d4f42255
Ben Blackman <bblkmn5@gmail.com>
2017-11-23 20:28:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,5 +30,10 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
-
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id, name, breed)
+  end
 end
