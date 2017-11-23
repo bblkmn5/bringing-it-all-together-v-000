00daf3b8fe976a4b8f8be0d6cb73fda4393c1910
Ben Blackman <bblkmn5@gmail.com>
2017-11-23 21:01:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,5 @@
+requre_relative "../config/evironment.rb"
+
 class Dog
   attr_accessor :id, :name, :breed
 
@@ -43,7 +45,7 @@ class Dog
 
   def self.find_by_id(id)
     sql = <<-SQL
-      SELECT * FROM dogs 
+      SELECT * FROM dogs
       WHERE id = ? LIMIT 1
       SQL
     DB[:conn].execute(sql, id).map do |row|
